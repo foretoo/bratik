02df33222c943b580098947d852f6c9e084812b6
Sergey Borovikov <serg.there@gmail.com>
2022-05-10 23:15:39 +0300

    add example round_shape

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,41 @@
-import "./style.css"
+import "./style.sass"
+import {
+  getcanvas, circle, shape, vertex, CLOSE, round_shape, clear, fill, stroke
+} from "./bratik"
+import { assign_value } from "./utils"
+import { Point } from "./bratik-types"
 
-const app = document.querySelector<HTMLDivElement>("#app")!
+const { canvas } = getcanvas(),
+      points: Point[] = [],
+      grey = "#ccc"
+
+let radius = 20
+
+canvas.onmousedown = (e: MouseEvent) => {
+  const point = { x: e.pageX, y: e.pageY }
+  assign_value(point, points, 5)
+  draw()
+}
+const radiusrange = document.querySelector("input")!
+radiusrange.oninput = (e: Event) => {
+  const target = e.target as HTMLInputElement
+  radius = parseInt(target.value)
+  draw()
+}
+
+const draw = () => {
+  clear()
+
+  stroke(grey, 0.5)
+  fill(null)
+  shape()
+  points.forEach((p) => vertex(p.x, p.y))
+  shape(CLOSE)
+
+  stroke(null)
+  fill(grey)
+  points.forEach((p) => circle(p.x, p.y, 3))
+
+  fill("blue")
+  points.length > 2 && round_shape(points, radius)
+}
