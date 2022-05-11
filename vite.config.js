96e0f9f254195467b7373beba3b12e7796c11414
Sergey Borovikov <serg.there@gmail.com>
2022-05-11 20:12:10 +0300

    move serve entry to src dir

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite"
+import path from "path"
 
 export default defineConfig({
   server: {
-    host: true
-  }
+    host: true,
+    open: "/src/index.html",
+  },
 })
