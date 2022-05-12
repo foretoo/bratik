768de254d4e46ec82f3331b37c6203054baaa19f
Sergey Borovikov <serg.there@gmail.com>
2022-05-12 16:36:45 +0300

    reset round_shape from mapping to reducing & add angle prop

--- src/lib/round-shape.ts
+++ src/lib/round-shape.ts
@@ -8,36 +8,40 @@ const round_shape = (
   points: Point[], radius: number
 ) => {
 
-  const rounded_points: LinkedRoundedPoint[] = points.map((curr, i) => {
+  const rounded_points: LinkedRoundedPoint[] =
+  points.reduce((shape: LinkedRoundedPoint[], curr, i) => {
     const
       prev = points[(i - 1 + points.length) % points.length],
       next = points[(i + 1) % points.length],
       length = find_length(curr, next),
-      angle_main = find_angle(prev, curr, next),
-      angle_next = (find_angle(next, curr) + PI) % TAU,
-      angle_prev = (find_angle(prev, curr) + PI) % TAU,
-      offset = radius / Math.tan(angle_main / 2),
-      bis_length = radius / Math.sin(angle_main / 2),
-      clock_dir = get_clock_dir(angle_prev, angle_next),
-      angle_bis = angle_prev + clock_dir * angle_main / 2
+      angle = {
+        main: find_angle(prev, curr, next),
+        next: (find_angle(next, curr) + PI) % TAU,
+        prev: (find_angle(prev, curr) + PI) % TAU,
+      },
+      offset = radius / Math.tan(angle.main / 2),
+      bis_length = radius / Math.sin(angle.main / 2),
+      clock_dir = get_clock_dir(angle.prev, angle.next),
+      angle_bis = angle.prev + clock_dir * angle.main / 2
 
-    return {
+    return shape.concat({
       id: i,
       ...curr,
       length,
       offset,
+      angle,
       radius: {
         length: radius,
         x: curr.x + Math.cos(angle_bis) * bis_length,
         y: curr.y + Math.sin(angle_bis) * bis_length,
       },
       in: {
-        x: curr.x + Math.cos(angle_prev) * offset,
-        y: curr.y + Math.sin(angle_prev) * offset,
+        x: curr.x + Math.cos(angle.prev) * offset,
+        y: curr.y + Math.sin(angle.prev) * offset,
       },
       out: {
-        x: curr.x + Math.cos(angle_next) * offset,
-        y: curr.y + Math.sin(angle_next) * offset,
+        x: curr.x + Math.cos(angle.next) * offset,
+        y: curr.y + Math.sin(angle.next) * offset,
       },
       get prev() {
         return rounded_points[(i - 1 + points.length) % points.length]
@@ -45,8 +49,8 @@ const round_shape = (
       get next() {
         return rounded_points[(i + 1) % points.length]
       }
-    }
-  })
+    })
+  }, [])
 
   return rounded_points
 }
