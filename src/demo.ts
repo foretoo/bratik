14b69ae08e89c9191d881ab117d443d41c79f517
Sergey Borovikov <serg.there@gmail.com>
2022-05-12 12:37:36 +0300

    provide rendering on rounded shape

--- src/demo.ts
+++ src/demo.ts
@@ -1,23 +1,26 @@
 import "./style.sass"
 import {
-  getcanvas, circle, round_shape, shape, vertex, CLOSE, clear, fill, stroke, Point
+  getcanvas, circle, round_shape, shape, vertex, CLOSE, clear, fill, stroke, Point, LinkedRoundedPoint
 } from "./lib/bratik"
 
 const { canvas } = getcanvas(),
       points: Point[] = [],
       grey = "#ccc"
 
-let radius = 20
+let radius = 20,
+    polygon: LinkedRoundedPoint[]
 
 canvas.onpointerdown = (e: PointerEvent) => {
   const point = { x: e.pageX, y: e.pageY }
   assign_value(point, points, 5)
+  polygon = round_shape(points, radius)
   draw()
 }
 const radiusrange = document.querySelector("input")!
 radiusrange.oninput = (e: Event) => {
   const target = e.target as HTMLInputElement
   radius = parseInt(target.value)
+  polygon = round_shape(points, radius)
   draw()
 }
 
@@ -34,8 +37,15 @@ const draw = () => {
   fill(grey)
   points.forEach((p) => circle(p.x, p.y, 3))
 
-  fill("blue")
-  points.length > 2 && round_shape(points, radius)
+  if (points.length > 2) {
+    polygon.forEach((point) => {
+      fill("blue")
+      circle(point.in.x, point.in.y, 3)
+      circle(point.out.x, point.out.y, 3)
+      fill("red")
+      circle(point.radius.point.x, point.radius.point.y, 3)
+    });
+  }
 }
 
 const assign_value = <T>(
