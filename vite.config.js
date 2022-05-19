09b673e96b61648432b0bd4e0dfce38c08756597
Sergey Borovikov <serg.there@gmail.com>
2022-05-19 17:39:03 +0300

    add cjs & iife output formats

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/lib/bratik.ts"),
       name: "bratik",
-      formats: [ "es" ]
+      formats: [ "es", "cjs", "iife" ]
     },
     target: "esnext",
     emptyOutDir: false,
