7dc712ced90fe2396d289dd5bbdc44f9fdaa0022
Sergey Borovikov <serg.there@gmail.com>
2022-05-21 15:14:44 +0300

    v0.3.2

--- dist/bratik.d.ts
+++ dist/bratik.d.ts
@@ -1,14 +1,7 @@
-declare const PI: number, TAU: number;
-declare const CLOSE = "close";
-
 declare type Point = { x: number; y: number };
-declare type RoundedPoint = Point & { angles: { main: number; next: number; prev: number; bis: number; dir: number }; radius: Point & { size: number; hit: number }; vel: number; offset: number; in: Point & { length: number; rest: number }; out: Point & { length: number; rest: number }; locked: boolean };
-declare type Linked<T> = T & { id: number; next: Linked<T>; prev: Linked<T> };
-declare type Render = (fn: () => void) => void;
-
+declare const PI: number, TAU: number, CLOSE = "close";
 declare let ctx: CanvasRenderingContext2D, frame: number, looping: boolean;
 declare const getcanvas: (w?: number | undefined, h?: number | undefined, id?: string | undefined) => { width: number; height: number; ctx: CanvasRenderingContext2D; canvas: HTMLCanvasElement };
-declare const round_shape: (points: Point[], radius: number) => Linked<RoundedPoint>[];
 declare const shape: (arg?: string | undefined) => void;
 declare const vertex: (x: number, y: number) => void;
 declare const arc: (x1: number, y1: number, x2: number, y2: number, r: number) => void;
@@ -23,6 +16,5 @@ declare const fill: (color: string | null) => void;
 declare const stroke: (color: string | null, width?: number | undefined) => void;
 declare const clear: (x?: number, y?: number, w?: number, h?: number) => void;
 declare let play: () => void, stop: typeof play;
-declare const loop: Render;
-
-export { getcanvas, ctx, shape, round_shape, vertex, arc, line, circle, rect, font, settext, text, fill, stroke, clear, draw, frame, loop, stop, looping, CLOSE, PI, TAU, type Point };
+declare const loop: (fn: () => void) => void;
+export { getcanvas, ctx, shape, vertex, arc, line, circle, rect, font, settext, text, fill, stroke, clear, draw, frame, loop, stop, looping, CLOSE, PI, TAU, type Point, };
