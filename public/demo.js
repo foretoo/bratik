26c4d81691ca997ebbc74d209b7aabf3a6a1a949
Sergey Borovikov <serg.there@gmail.com>
2022-05-21 15:27:09 +0300

    restructured

--- public/demo.js
+++ public/demo.js
@@ -1,12 +1,6 @@
-import { getcanvas, circle, round_shape, shape, vertex, CLOSE, clear, fill, stroke, arc } from "../dist/bratik.es.js";
-const { canvas } = getcanvas(), points = [
-// { x: 157, y: 210 },{ x: 265, y: 202 },{ x: 304, y: 363 },{ x: 100, y: 373 },{ x: 221, y: 501 },
-// { x: 230, y: 295 },{ x: 280, y: 172 },{ x: 112, y: 267 },{ x: 152, y: 493 },{ x: 339, y: 447 },
-// { x: 280, y: 172 },{ x: 112, y: 267 },{ x: 152, y: 493 },{ x: 339, y: 447 },{ x: 112, y: 370 },
-// { x: 218, y: 420 },{ x: 415, y: 287 },{ x: 252, y: 314 },{ x: 135, y: 126 },{ x: 142, y: 414 },
-// { x: 165, y: 502 },{ x: 345, y: 417 },{ x: 351, y: 277 },{ x: 238, y: 261 },{ x: 214, y: 65 },
-// { x: 265, y: 396 },{ x: 239, y: 204 },{ x: 132, y: 435 },{ x: 309, y: 543 },{ x: 412, y: 163 },
-], grey = "#0007", bluish = "#00f7";
+import { getcanvas, circle, shape, vertex, CLOSE, clear, fill, stroke, arc } from "../dist/bratik.es.js";
+import { round_shape } from "./round-shape.js";
+const { canvas } = getcanvas(), points = [], grey = "#0007", bluish = "#00f7";
 let polygon = [];
 canvas.onpointerdown = (e) => {
     const point = { x: e.pageX, y: e.pageY };
@@ -44,35 +38,6 @@ const draw = () => {
             vertex(p.next.in.x, p.next.in.y);
         });
         shape(CLOSE);
-        // polygon.forEach((p, i) => {
-        //   //// Centers of roundings
-        //   stroke(null)
-        //   fill("blue")
-        //   circle(p.radius.x!, p.radius.y!, 3)
-        //   //// Arcs of roundings, stroked
-        //   fill(null)
-        //   stroke("black", 3)
-        //   shape()
-        //   vertex(p.in!.x, p.in!.y)
-        //   arc(p.x, p.y, p.out!.x, p.out!.y, p.radius.size);
-        //   shape()
-        //   //// Points numbers
-        //   stroke(null)
-        //   fill(grey)
-        //   font(14)
-        //   settext("center", "middle")
-        //   const { bis, dir } = p.angles,
-        //         x = p.x - dir * Math.cos(bis) * 24,
-        //         y = p.y - dir * Math.sin(bis) * 24
-        //   text(`${i}`, x, y)
-        //   font(10)
-        //   const inx = x - dir * Math.cos(bis + PI / 2) * 24,
-        //         iny = y - dir * Math.sin(bis + PI / 2) * 24
-        //   text(`in`, inx, iny)
-        //   const outx = x - dir * Math.cos(bis - PI / 2) * 24,
-        //         outy = y - dir * Math.sin(bis - PI / 2) * 24
-        //   text(`out`, outx, outy)
-        // })
     }
 };
 const assign_value = (value, arr, length) => {
