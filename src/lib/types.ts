93a3dbe06399b1846dee288b3a650c6c80c7cc92
Sergey Borovikov <serg.there@gmail.com>
2022-05-29 22:37:32 +0300

    del Point, RoundedPoint

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,23 +2,6 @@ export type Point = {
   x: number,
   y: number,
 }
-export type RoundedPoint = Point & {
-  angles: { main: number, next: number, prev: number, bis: number, dir: number },
-  radius: Point & { size: number, hit: number },
-  vel: number,
-  offset: number,
-  in: Point & { length: number, rest: number },
-  out: Point & { length: number, rest: number },
-  locked: boolean,
-}
-export type Linked<T> = T & {
-  id: number,
-  next: Linked<T>,
-  prev: Linked<T>,
-}
-
-
-
 export type FindLength = (
   A: Point, B: Point,
 ) => number
