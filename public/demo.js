1658794f4e7de5a89f6a92bd5764f1f8116109a9
Sergey Borovikov <serg.there@gmail.com>
2022-05-30 03:22:34 +0300

    linted

--- public/demo.js
+++ public/demo.js
@@ -1,52 +1,48 @@
-import { getcanvas, circle, shape, vertex, CLOSE, clear, fill, stroke, arc } from "../dist/bratik.es.js";
-import round_shape from "https://unpkg.com/round-polygon@0.5.1/lib/round-polygon.es.js";
-const { canvas } = getcanvas(), points = [], grey = "#0007", bluish = "#00f7";
-let polygon = [];
+import { getcanvas, circle, shape, vertex, CLOSE, clear, fill, stroke, arc } from "../lib/bratik.es.js"
+import round_shape from "https://unpkg.com/round-polygon@latest/lib/round-polygon.es.js"
+const { canvas } = getcanvas(), points = [], grey = "#0007", bluish = "#00f7"
+let polygon = []
 canvas.onpointerdown = (e) => {
-    const point = { x: e.pageX, y: e.pageY };
-    assign_value(point, points, 9);
-    polygon = round_shape(points, +radiusrange.value);
-    draw();
-};
-const radiusrange = document.querySelector("input");
-const radiusvalue = document.querySelector("#radiusvalue");
-radiusvalue.textContent = radiusrange.value;
+  const point = { x: e.pageX, y: e.pageY }
+  assign_value(point, points, 9)
+  polygon = round_shape(points, +radiusrange.value)
+  draw()
+}
+const radiusrange = document.querySelector("input")
+const radiusvalue = document.querySelector("#radiusvalue")
+radiusvalue.textContent = radiusrange.value
 radiusrange.oninput = (e) => {
-    const target = e.target;
-    radiusvalue.textContent = target.value;
-    polygon = round_shape(points, +radiusrange.value);
-    draw();
-};
+  const target = e.target
+  radiusvalue.textContent = target.value
+  polygon = round_shape(points, +radiusrange.value)
+  draw()
+}
 const draw = () => {
-    clear();
-    stroke(grey, 0.5);
-    fill(null);
-    shape();
-    points.forEach((p) => vertex(p.x, p.y));
-    shape(CLOSE);
-    stroke(null);
-    fill(grey);
-    points.forEach((p) => circle(p.x, p.y, 1));
-    if (points.length > 2) {
-        fill(bluish);
-        stroke("blue", 1);
-        shape();
-        polygon.forEach((p, i) => {
-            if (!i)
-                vertex(p.in.x, p.in.y);
-            arc(p.x, p.y, p.next.x, p.next.y, p.arc.radius);
-            vertex(p.next.in.x, p.next.in.y);
-        });
-        shape(CLOSE);
-    }
-};
+  clear()
+  stroke(grey, 0.5)
+  fill(null)
+  shape()
+  points.forEach((p) => vertex(p.x, p.y))
+  shape(CLOSE)
+  stroke(null)
+  fill(grey)
+  points.forEach((p) => circle(p.x, p.y, 1))
+  if (points.length > 2) {
+    fill(bluish)
+    stroke("blue", 1)
+    shape()
+    polygon.forEach((p, i) => {
+      if (!i) vertex(p.in.x, p.in.y)
+      arc(p.x, p.y, p.next.x, p.next.y, p.arc.radius)
+      vertex(p.next.in.x, p.next.in.y)
+    })
+    shape(CLOSE)
+  }
+}
 const assign_value = (value, arr, length) => {
-    if (arr.length < length)
-        arr.push(value);
-    else {
-        for (let i = 0; i < arr.length - 1; i++) {
-            arr[i] = arr[i + 1];
-        }
-        arr[length - 1] = value;
-    }
-};
+  if (arr.length < length) arr.push(value)
+  else {
+    for (let i = 0; i < arr.length - 1; i++) arr[i] = arr[i + 1]
+    arr[length - 1] = value
+  }
+}
