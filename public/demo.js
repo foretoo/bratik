327074978d655ed5e0cabcdbc4e07a4c7da0db10
Sergey Borovikov <serg.there@gmail.com>
2022-06-17 16:24:51 +0300

    add gradient

--- public/demo.js
+++ public/demo.js
@@ -1,13 +1,19 @@
-import { getcanvas, circle, shape, vertex, CLOSE, clear, fill, stroke, arc } from "../lib/bratik.es.js"
+import { getcanvas, circle, shape, vertex, CLOSE, clear, fill, stroke, gradient, arc } from "../lib/bratik.es.js"
 import round_shape from "https://unpkg.com/round-polygon@latest/lib/round-polygon.es.js"
-const { canvas } = getcanvas(), points = [], grey = "#0007", bluish = "#00f7"
-let polygon = []
+const { canvas, width } = getcanvas(), points = [], grey = "#0007", bluish = "#00f7", solo = gradient("LINEAR", 0, 0, width, 0)
+const solocolors = Array(width/20).fill(null).map(() => {
+  const v = Math.random()*255|0
+  return { p: Math.random(), color: `rgb(${v},${v},${v})`}
+})
+let polygon
+
 canvas.onpointerdown = (e) => {
   const point = { x: e.pageX, y: e.pageY }
   assign_value(point, points, 9)
   polygon = round_shape(points, +radiusrange.value)
   draw()
 }
+
 const radiusrange = document.querySelector("input")
 const radiusvalue = document.querySelector("#radiusvalue")
 radiusvalue.textContent = radiusrange.value
@@ -17,19 +23,15 @@ radiusrange.oninput = (e) => {
   polygon = round_shape(points, +radiusrange.value)
   draw()
 }
+
 const draw = () => {
   clear()
-  stroke(grey, 0.5)
-  fill(null)
-  shape()
-  points.forEach((p) => vertex(p.x, p.y))
-  shape(CLOSE)
-  stroke(null)
-  fill(grey)
-  points.forEach((p) => circle(p.x, p.y, 1))
+
   if (points.length > 2) {
-    fill(bluish)
-    stroke("blue", 1)
+    solo.reset()
+    solocolors.forEach(({ p, color }) => solo.add(p, color))
+    fill(solo.image)
+    stroke("blue", 2)
     shape()
     polygon.forEach((p, i) => {
       if (!i) vertex(p.in.x, p.in.y)
@@ -38,11 +40,23 @@ const draw = () => {
     })
     shape(CLOSE)
   }
+
+  stroke(grey, 0.5)
+  fill(null)
+  shape()
+  points.forEach((p) => vertex(p.x, p.y))
+  shape(CLOSE)
+
+  stroke(null)
+  fill(grey)
+  points.forEach((p) => circle(p.x, p.y, 1))
 }
+
 const assign_value = (value, arr, length) => {
   if (arr.length < length) arr.push(value)
   else {
-    for (let i = 0; i < arr.length - 1; i++) arr[i] = arr[i + 1]
+    for (let i = 0; i < arr.length - 1; i++)
+      arr[i] = arr[i + 1]
     arr[length - 1] = value
   }
 }
